96c80ae78f47018114d19bad3efbd83a703ee407
Kristen Krofchik <k.krofchik@gmail.com>
2021-02-03 18:57:19 -0600

    passing test_message_model

--- test_message_model.py
+++ test_message_model.py
@@ -6,8 +6,8 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
-from sqlalchemy import exc
+from models import db, User, Message, Follows, Likes
+#from sqlalchemy import exc
 
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
@@ -56,5 +56,36 @@ class MessageModelTestCase(TestCase):
 
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 1)
-        self.assertEqual(self.u.messages[0].text, "This is a test.")
+        self.assertEqual(u.messages[0].text, "This is a test.")
+
+    def test_message_likes(self):
+        msg_1 = Message(
+            text="I love pizza!",
+            user_id=self.uid
+        )
+
+        msg_2 = Message(
+            text='I love candy!',
+            user_id=self.uid
+        )
+
+        new_user = User.signup("sohungry", "food@food.com", "foodpassword", None)
+        uid = 888
+        new_user.id = uid
+
+        db.session.add(msg_1)
+        db.session.add(msg_2)
+        db.session.add(new_user)
+        db.session.commit()
+
+        new_user.likes.append(msg_1)
+        db.session.commit()
+
+        like_list = Likes.query.filter(Likes.user_id == uid).all()
+        self.assertEqual(len(like_list), 1)
+
+        nolikes_list = Likes.query.filter(Likes.user_id == self.uid).all()
+        self.assertEqual(len(nolikes_list), 0)
+
+
       
